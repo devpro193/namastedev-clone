1efa1aab4bd0ea37ad36523934a94b37ce0ce4f5
Rakshit <404rakshit@gmail.com>
2024-03-24 20:02:21 +0530

    scroll-animation

--- src/components/home-components/banner.tsx
+++ src/components/home-components/banner.tsx
@@ -5,16 +5,28 @@ export default function Banner() {
   return (
     <Unveal>
       <div className={`relative font-jakarta tab:min-h-16`}>
-        <div className="tab:hidden tab:min-h-16" />
+        <div className="min-h-12" />
         <Link
           href={"https://courses.30dayscoding.com/s/store"}
           target="_blank"
-          className={`bg-gradient-to-r from-second via-head to-second animate-gradient bg-[length:200%_auto] font-bold flex items-center justify-center text-lg max-tab:hidden text-white tab:min-h-16 fixed tab:top-0 w-full z-20`}
+          className={`bg-gradient-to-r from-second via-head to-second animate-gradient bg-[length:200%_auto] font-bold flex items-center justify-center text-lg text-white min-h-12 fixed top-0 w-full z-20`}
         >
-          <p className="tracking-wide text-center">
+          {/* <p className="tracking-wide text-center">
             Apply now and get our Docker crash course for FREE 🎁
             <span className="underline mx-2">Apply now!</span>
-          </p>
+          </p> */}
+
+          <section className="container">
+            <p className="loop-text">
+              ✨ Flat 50% off on all Crash courses and Self-paced programs
+            </p>
+            <p className="loop-text">
+              ✨ Flat 50% off on all Crash courses and Self-paced programs
+            </p>
+            <p className="loop-text">
+              ✨ Flat 50% off on all Crash courses and Self-paced programs
+            </p>
+          </section>
         </Link>
       </div>
     </Unveal>
